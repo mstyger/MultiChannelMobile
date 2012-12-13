6602868f34f7dd2e0449cc48db07750a162a1431
Marcel Styger <marcel.styger@gmail.com>
2012-12-13 14:56:14 +0100

    add comments

--- src/ch/zh/dipima/multichannelmobile/ShowLog.java
+++ src/ch/zh/dipima/multichannelmobile/ShowLog.java
@@ -1,3 +1,9 @@
+/**
+ * @author: Pius Fonseka, Dieter Good, Marcel Styger (BWI A-11)
+ * @version 1.0
+ * @description: Android App for HWZ Project, 2012
+ */
+
 package ch.zh.dipima.multichannelmobile;
 
 import java.io.FileInputStream;
